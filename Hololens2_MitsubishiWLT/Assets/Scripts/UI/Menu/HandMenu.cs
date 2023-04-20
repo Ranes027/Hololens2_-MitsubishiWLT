4a3986cb5beebdf1d7753c45b30c799408dee146
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-04-20 17:12:43 +0300

    Create HandMenu.cs

---
+++ Hololens2_MitsubishiWLT/Assets/Scripts/UI/Menu/HandMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MitsubishiAR.UI
+{
+    public class HandMenu : MenuWindow
+    {
+        [SerializeField] private GameObject _adminButton;
+        [SerializeField] private GameObject _mainMenuButton;
+
+        public void MainMenuMode()
+        {
+            _mainMenuButton.SetActive(false);
+            _adminButton.SetActive(true);
+        }
+
+        public void OutOfMainMenuMode()
+        {
+            _mainMenuButton.SetActive(true);
+            _adminButton.SetActive(false);
+        }
+    }
+
+}
