dd1e3c2d0cc55ec07f565af8a12fd06a5da82630
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-04-20 17:11:23 +0300

    Add new folder

--- Hololens2_MitsubishiWLT/Assets/Scripts/UI/Menu/MainMenu.cs
+++ Hololens2_MitsubishiWLT/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -1,17 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MitsubishiAR.StateMachine;
 using static MitsubishiAR.Components.Object.States.ActiveStateComponent;
 
 namespace MitsubishiAR.UI
 {
-    public class MainMenu : MonoBehaviour
+    public class MainMenu : MenuWindow
     {
         [SerializeField] private GameObject _general;
         [SerializeField] private GameObject _login;
         [SerializeField] private GameObject _settings;
 
         [SerializeField] private GameObject _backButton;
+
+        public void ChangeStateFromMenu(string stateName)
+        {
+            var sceneContent = FindObjectOfType<SceneContent>();
+            if (sceneContent != null)
+            {
+                switch (stateName)
+                {
+                    case "Info":
+                        sceneContent.SceneSM.ChangeState(sceneContent.Info);
+                        Debug.Log("Scene changed to Info");
+                        break;
+                    case "StandTurnOn":
+                        sceneContent.SceneSM.ChangeState(sceneContent.StandTurnOn);
+                        break;
+                    case "ControllerCheckUp":
+                        sceneContent.SceneSM.ChangeState(sceneContent.StandTurnOn);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Debug.Log("Add SceneContent to scene!");
+            }
+
+        }
 
         public void Login()
         {
@@ -44,8 +73,6 @@ namespace MitsubishiAR.UI
                 ChangeState(_objects);
             }
         }
-
-        
     }
 
 }
