bf19155a473df7b5c335250d9b5bedc080dfd1ae
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-04-22 18:30:28 +0300

    Create StandInstructionMenu.cs

---
+++ Hololens2_MitsubishiWLT/Assets/Scripts/UI/Menu/StandInstructionMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MitsubishiAR.StateMachine;
+
+namespace MitsubishiAR.UI
+{
+    public class StandInstructionMenu : MenuWindow
+    {
+    }
+
+}
