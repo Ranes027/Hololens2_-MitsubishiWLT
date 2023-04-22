206168b6ab2f2db2ea48c0e5c6e06f0d2edd8b89
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-04-22 18:29:09 +0300

    Update SceneContent.cs

--- Hololens2_MitsubishiWLT/Assets/Scripts/StateMachine/SceneContent.cs
+++ Hololens2_MitsubishiWLT/Assets/Scripts/StateMachine/SceneContent.cs
@@ -13,8 +13,12 @@ namespace MitsubishiAR.StateMachine
         [SerializeField] private MainMenu _mainMenu;
         [SerializeField] private AdminMenu _adminMenu;
         [SerializeField] private HandMenu _handMenu;
+        [SerializeField] private StandInstructionMenu _standMenu;
+        [Header("Animator Controller Settings")]
+        [SerializeField] private RuntimeAnimatorController _sceneContentAnimator;
         [Header("Scene Objects")]
         [SerializeField] private GameObject[] _tips;
+        [SerializeField] private GameObject[] _standInstObjects;
         [Space]
         public StateMachine SceneSM;
         public IdleState Idle;
@@ -26,10 +30,21 @@ namespace MitsubishiAR.StateMachine
         public MainMenu MainMenu => _mainMenu;
         public AdminMenu AdminMenu => _adminMenu;
         public HandMenu HandMenu => _handMenu;
+        public StandInstructionMenu StandMenu => _standMenu;
 
         public GameObject[] Tips => _tips;
+        public GameObject[] StandInstObjects => _standInstObjects;
 
         private Coroutine _coroutine;
+        private Animator _animator;
+        private Animator Animator => _animator;
+
+        public RuntimeAnimatorController SceneContentAnimator => _sceneContentAnimator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
 
         private void Start()
         {
@@ -59,7 +74,13 @@ namespace MitsubishiAR.StateMachine
             SceneSM.CurrentState.LateEnter();
         }
 
-
+        public void UpdateSceneAnimator(RuntimeAnimatorController animator)
+        {
+            if(_animator.runtimeAnimatorController != animator)
+            {
+                _animator.runtimeAnimatorController = animator;
+            }
+        }
     }
 
 }
